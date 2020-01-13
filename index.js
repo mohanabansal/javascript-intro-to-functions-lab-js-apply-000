1d147d5ff10ef15f3ba781316ba6c0fda0e50dd9
Mohana Bansal <mohanabansal10@gmail.com>
2020-01-13 21:10:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,19 +14,6 @@ function logWhisper(string){
   console.log( string.toLowerCase());
 }
 
-function sayHiToGrandma(string){
-  if(string.toLowerCase() == string) return "I can't hear you!"
-}
-
-function sayHiToGrandma(string){
-  if(string.toUpperCase() === string){
-    return ("YES INDEED!")
-  }
-}
-
-function sayHiToGrandma(string){
-  if(string === "I love you, Grandma.") return "I love you, too."
-}
 
 
 
